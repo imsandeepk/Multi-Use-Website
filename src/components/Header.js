f007e5b93c0690641eb20dd1fbb1734afbe92d94
Sandeep Kumar <65333883+imsandeepk@users.noreply.github.com>
2023-06-10 01:14:31 +0100

    cheems

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <nav className="navbar navbar-expand-lg bg-black ">
   <div className="container-fluid ">
-    <img src="'../public/chems.png" className='img-thumbnail'/>
+    <img src="/chems.png" className='img-thumbnail h-10'/>
     <a className="navbar-brand text-white" href="/">Multi-Use Website</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
